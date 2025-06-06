1b72eca955b5d14e3c160147dac33d64504bf9ee
GitGaurav1 <159174211+GitGaurav1@users.noreply.github.com>
2025-06-06 16:14:40 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: '/gaurav-mathur-portfolio/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
-}));
+export default defineConfig({
+  base: '/gaurav-mathur-portfolio/',  // 👈 this is required for GitHub Pages
+  plugins: [react()],
+})
